44fc5600b41325a4fb0edb6f8ad6aa5a9a3fb69c
Jeremy Neander <subjectego@gmail.com>
2012-07-09 12:01:42 -0500

    Separate tests into contexts to describe preconditions to the tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,30 +1,42 @@
-require 'mock_player'
 require 'game'
 
 describe Game do
   before :each do
-    @board = Board.new(3)
     @console = stub("console")
     @game = Game.new(@console)
-    @game.board = @board
-    @player1 = MockPlayer.new
+    @player1 = mock("player")
   end
 
-  it "should not be over after creation" do
-    @game.over?.should eql false
-  end
-
-  it "should end when the board has a winning solution" do
-    @game.players << @player1
-    [0,1,2].each do |index|
-      @board.make_mark(index, @player1)
+  context "with new board" do
+    before :each do
+      @game.board = stub("board").as_null_object
+      @game.board.stub(:winning_solution?).and_return(false)
+      @game.board.stub(:spaces_with_mark).and_return([nil]*9)
     end
-    @game.over?.should eql true
+
+    it "should not be over after creation" do
+      @game.over?.should eql false
+    end
   end
 
-  it "should request a mark from the player" do
-    @game.players << @player1
-    @game.run
-    @player1.mark_requested.should eql true
+  context "while not over" do
+    it "should request a mark from the player" do
+      @game.players << @player1
+      @player1.should_receive(:make_mark)
+      @game.run
+    end
+  end
+
+  context "with winning board" do
+    it "should end when the board has a winning solution" do
+      set_winning_board
+      @game.over?.should eql true
+    end
+  end
+
+  private
+  def set_winning_board(mark = :player)
+    @game.board = stub("board").as_null_object
+    @game.board.stub!(:winning_solution?).and_return(true)
   end
 end
