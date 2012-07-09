d74c6ee297a92b4a7fe21dc16e5573f0d3dbd3bf
Jeremy Neander <subjectego@gmail.com>
2012-07-09 12:05:14 -0500

    Change test descriptions to describe what occurs in each test, not what 'should' occur

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -14,13 +14,13 @@ describe Game do
       @game.board.stub(:spaces_with_mark).and_return([nil]*9)
     end
 
-    it "should not be over after creation" do
+    it "is not over after creation" do
       @game.over?.should eql false
     end
   end
 
   context "while not over" do
-    it "should request a mark from the player" do
+    it "requests a mark from the player" do
       @game.players << @player1
       @player1.should_receive(:make_mark)
       @game.run
@@ -28,7 +28,7 @@ describe Game do
   end
 
   context "with winning board" do
-    it "should end when the board has a winning solution" do
+    it "ends when the board has a winning solution" do
       set_winning_board
       @game.over?.should eql true
     end
