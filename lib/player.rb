8a1f761970a3fed9c0149b05f4fd5579aca6d5f5
Jeremy Neander <subjectego@gmail.com>
2012-07-09 22:07:27 -0500

    Make 'Player' verify that a mark can be made before writing changes to the board

--- lib/player.rb
+++ lib/player.rb
@@ -3,5 +3,9 @@ class Player
 
   def make_mark(board)
     index = @console.prompt_player_mark
+    while not board.is_available_space?(index)
+      index = @console.prompt_player_mark
+    end
+    board.make_mark(index,self)
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,14 +2,21 @@ require 'player'
 
 describe Player do
   before :each do
-    @console = mock("console")
-    @board = mock("board")
+    @console = mock("console").as_null_object
+    @board = mock("board").as_null_object
     @player = Player.new
     @player.console = @console
   end
 
   it "requests mark information from the console" do
     @console.should_receive("prompt_player_mark").and_return(0)
+    @player.make_mark(@board)
+  end
+
+  it "checks for valid mark information" do
+    @console.should_receive(:prompt_player_mark).and_return(0,1)
+    @board.should_receive(:make_mark).once
+    @board.should_receive(:is_available_space?).and_return(false,true)
     @player.make_mark(@board)
   end
 end
