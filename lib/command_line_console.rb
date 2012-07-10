66c811bca8ce5308d7a159dd0d1ff01ad4675d5c
Jeremy Neander <subjectego@gmail.com>
2012-07-10 15:30:29 -0500

    Create method to print the board and available spaces to the standard output

--- lib/command_line_console.rb
+++ lib/command_line_console.rb
@@ -11,6 +11,14 @@ class CommandLineConsole
     @characters[players.last] = "X"
   end
 
+  def display_board(board)
+    print_board = convert_board_to_ascii(board)
+    print_available = available_spaces_to_ascii(board)
+    (0...board.size).each {|index| 
+      printf("%10s%10s\n", print_board[index], print_available[index])
+    }
+  end
+
   def convert_board_to_ascii(board)
     board.spaces.each_slice(board.size).collect {|row| row.collect {
       |mark| @characters[mark]}.join("|")}
