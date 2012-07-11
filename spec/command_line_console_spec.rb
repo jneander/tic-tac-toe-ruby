39d4b4df761dd33c2b8faffffadbfc69bfa0e1a9
Jeremy Neander <subjectego@gmail.com>
2012-07-11 10:39:05 -0500

    Add test for command-line input

--- spec/command_line_console_spec.rb
+++ spec/command_line_console_spec.rb
@@ -8,12 +8,18 @@ describe "CommandLineConsole" do
     @console.set_players(@players)
     @spaces_blank = [Mark::BLANK]*9
     @spaces_with_marks = [Mark::BLANK,@players.first,@players.last]*3
+    $stdout = StringIO.new('','w')
   end
 
   it "assigns ASCII characters to players and marks in 'Game'" do
     @console.characters[:player1].should eql 'O'
     @console.characters[:player2].should eql 'X'
     @console.characters[Mark::BLANK].should eql '_'
+  end
+
+  it "receives command-line input when prompted" do
+    $stdin = StringIO.new('2','r')
+    @console.prompt_player_mark.should eql 1
   end
 
   it "creates an array of strings representing the board" do
