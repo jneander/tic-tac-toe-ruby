8d723e0d64db5b960a1d7d74102b87a539cbd0c3
Jeremy Neander <subjectego@gmail.com>
2012-07-10 23:12:37 -0500

    Add call to alert the user when the chosen space is unavailable on the board

--- lib/player.rb
+++ lib/player.rb
@@ -4,6 +4,7 @@ class Player
   def make_mark(board)
     index = @console.prompt_player_mark
     while not board.space_available?(index)
+      @console.alert_space_unavailable(index)
       index = @console.prompt_player_mark
     end
     board.make_mark(index,self)
