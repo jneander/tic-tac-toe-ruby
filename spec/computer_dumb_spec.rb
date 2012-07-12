c957004953c0118c9339874850d2cfa96ba95800
Jeremy Neander <subjectego@gmail.com>
2012-07-12 12:22:01 -0500

    Place existing tests within the context of 'making marks'

--- spec/computer_dumb_spec.rb
+++ spec/computer_dumb_spec.rb
@@ -5,30 +5,32 @@ ALL_SPACES = (0..8).sort
 SOME_SPACES = (3..6).sort
 
 describe DumbComputer do
-  before :each do
-    @computer = DumbComputer.new
-    @board = mock("board")
-    @board.should_receive(:make_mark).exactly(TIMES).times
-    @marks = []
-  end
-
-  it "makes a mark at random" do
-    @board.should_receive(:spaces_with_mark).exactly(TIMES).times
-      .and_return(ALL_SPACES)
-
-    TIMES.times do
-      @marks << @computer.make_mark(@board)
+  context "when making marks" do
+    before :each do
+      @computer = DumbComputer.new
+      @board = mock("board")
+      @board.should_receive(:make_mark).exactly(TIMES).times
+      @marks = []
     end
-    @marks.uniq.sort.should eql ALL_SPACES
-  end
 
-  it "marks only on unmarked spaces" do
-    @board.should_receive(:spaces_with_mark).exactly(TIMES).times
-      .and_return(SOME_SPACES)
+    it "makes a mark at random" do
+      @board.should_receive(:spaces_with_mark).exactly(TIMES).times
+        .and_return(ALL_SPACES)
 
-    TIMES.times do
-      @marks << @computer.make_mark(@board)
+      TIMES.times do
+        @marks << @computer.make_mark(@board)
+      end
+      @marks.uniq.sort.should eql ALL_SPACES
     end
-    @marks.uniq.sort.should eql SOME_SPACES
+
+    it "marks only on unmarked spaces" do
+      @board.should_receive(:spaces_with_mark).exactly(TIMES).times
+        .and_return(SOME_SPACES)
+
+      TIMES.times do
+        @marks << @computer.make_mark(@board)
+      end
+      @marks.uniq.sort.should eql SOME_SPACES
+    end
   end
 end
