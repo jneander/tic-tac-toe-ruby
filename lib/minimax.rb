8405f68d7d80f554a24dab7eb9ee8935b60e5e3f
Jeremy Neander <subjectego@gmail.com>
2012-07-18 13:27:00 -0500

    Make 'score' return -1 for winning opponent move

--- lib/minimax.rb
+++ lib/minimax.rb
@@ -1,7 +1,10 @@
 class Minimax
+  attr_accessor :max_player, :min_player
+
   def score(board,player)
     score = 0
-    score = 1 if board.winning_solution?(player)
+    score = 1 if board.winning_solution?(@max_player)
+    score = -1 if board.winning_solution?(@min_player)
     score
   end
 end
--- spec/minimax_spec.rb
+++ spec/minimax_spec.rb
@@ -6,17 +6,25 @@ describe Minimax do
   end
   
   before :each do
-    @player1 = mock("Player")
-    @board = mock("Board")
+    @min_player = mock("Player")
+    @max_player = mock("Player")
+    @board = mock("Board").as_null_object
+    @solver.min_player = @min_player
+    @solver.max_player = @max_player
   end
 
   it "scores high on winning move" do
-    @board.should_receive(:winning_solution?).with(@player1).and_return(true)
-    @solver.score(@board,@player1).should == 1
+    @board.should_receive(:winning_solution?).with(@max_player).and_return(true)
+    @solver.score(@board,@max_player).should == 1
   end
 
   it "scores zero on non-winning move" do
     @board.stub!(:winning_solution?).and_return(false)
-    @solver.score(@board,@player1).should == 0
+    @solver.score(@board,@max_player).should == 0
+  end
+
+  it "scores low on opponent's winning move" do
+    @board.should_receive(:winning_solution?).with(@min_player).and_return(true)
+    @solver.score(@board,@min_player).should == -1
   end
 end
