9bc8282dcf519b915b3c22987df87e5582aea9b1
Jeremy Neander <subjectego@gmail.com>
2012-07-18 17:17:00 -0500

    Replace mock board with 'Board' class instance

--- spec/minimax_spec.rb
+++ spec/minimax_spec.rb
@@ -1,4 +1,5 @@
 require 'minimax'
+require 'board'
 
 describe Minimax do
   before :all do
@@ -6,9 +7,9 @@ describe Minimax do
   end
   
   before :each do
+    @board = Board.new
     @min_player = mock("Player")
     @max_player = mock("Player")
-    @board = mock("Board").as_null_object
     @solver.min_player = @min_player
     @solver.max_player = @max_player
   end
@@ -16,30 +17,34 @@ describe Minimax do
   it "scores high on winning mark" do
     @board.should_receive(:winning_solution?).with(@max_player)
       .and_return(true)
+    @board.should_receive(:winning_solution?).with(@min_player)
+      .and_return(false)
     @solver.score(@board,@max_player).should == 1
   end
 
   it "scores zero on non-winning mark" do
-    @board.stub!(:winning_solution?).and_return(false)
+    use_no_winning_solution
     @board.stub!(:spaces_with_mark).and_return([])
     @solver.score(@board,@max_player).should == 0
   end
 
   it "scores low on opponent's winning mark" do
+    @board.should_receive(:winning_solution?).with(@max_player)
+      .and_return(false)
     @board.should_receive(:winning_solution?).with(@min_player)
       .and_return(true)
     @solver.score(@board,@min_player).should == -1
   end
 
   it "makes mark if no solution but spaces available" do
-    @board.stub!(:winning_solution?).and_return(false)
+    use_no_winning_solution
     @board.stub!(:spaces_with_mark).and_return([1,2,3],[])
     @board.should_receive(:make_mark) {|space| space.should == 1}
     @solver.score(@board,@min_player)
   end
 
   it "calls 'score' recursively until no spaces available" do
-    @board.stub!(:winning_solution?).and_return(false)
+    use_no_winning_solution
     @board.should_receive(:spaces_with_mark)
       .and_return([1,2,3],[2,3],[3],[])
     @solver.score(@board,@min_player)
@@ -57,5 +62,10 @@ describe Minimax do
     @board.stub!(:winning_solution?).with(@min_player).and_return(false,true)
     @board.stub!(:spaces_with_mark).and_return([1,2,3])
     @solver.score(@board,@min_player).should == -1
+  end
+
+  private
+  def use_no_winning_solution
+    @board.stub!(:winning_solution?).and_return(false)
   end
 end
