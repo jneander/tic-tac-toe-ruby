a505d54cbe21c4320224339f2c00bfa95ef787e7
Jeremy Neander <subjectego@gmail.com>
2012-07-19 09:15:47 -0500

    Add method to return last available space to mark

--- lib/computer_impossible.rb
+++ lib/computer_impossible.rb
@@ -1,5 +1,11 @@
+require 'mark'
+
 class ImpossibleComputer
   attr_accessor :console
+
+  def get_best_space(board)
+    board.spaces_with_mark(Mark::BLANK)[0]
+  end
 
   def self.to_s
     "Impossible Computer"
--- spec/computer_impossible_spec.rb
+++ spec/computer_impossible_spec.rb
@@ -1,12 +1,24 @@
 require 'computer_impossible'
 
 describe ImpossibleComputer do
+  before :all do
+    @computer = ImpossibleComputer.new
+  end
+
+  before :each do
+    @board = mock("Board")
+  end
+
   it "converts the class to a string" do
     ImpossibleComputer.to_s.should == "Impossible Computer"
   end
 
   it "can receive and store a reference to the console" do
-    @computer = ImpossibleComputer.new
     @computer.console = mock("Console")
+  end
+
+  it "returns the last available space to mark" do
+    @board.stub!(:spaces_with_mark).and_return([8])
+    @computer.get_best_space(@board).should == 8
   end
 end
