17c28e3d20cec053453c0b38ca01371acaaaccb4
Jeremy Neander <subjectego@gmail.com>
2012-07-20 09:14:30 -0500

    Add method 'score' to return value of board mark
    Minimax.score should return 1 if the designated max_mark is the
    winning mark on the board.

--- lib/minimax.rb
+++ lib/minimax.rb
@@ -1,3 +1,7 @@
 class Minimax
   attr_accessor :min_mark, :max_mark
+
+  def score(board,mark)
+    return 1
+  end
 end
--- spec/minimax_spec.rb
+++ spec/minimax_spec.rb
@@ -1,12 +1,17 @@
 require 'minimax'
+require 'board'
 
 describe Minimax do
-  before :each do
+  before :all do
     @minimax = Minimax.new
+    @minimax.max_mark = :max_mark
+    @minimax.min_mark = :min_mark
   end
 
-  it "stores references to 'min' and 'max' marks" do
-    @minimax.max_mark = :MAX
-    @minimax.min_mark = :MIN
+  it "returns 1 for max_mark win" do
+    @board = Board.new
+    @board.stub!(:winning_solution?).with(:min_mark).and_return(false)
+    @board.stub!(:winning_solution?).with(:max_mark).and_return(true)
+    @minimax.score(@board,:max_mark).should == 1
   end
 end
