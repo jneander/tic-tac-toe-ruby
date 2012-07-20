a38a052c44c9ebff4b60a137c912aabda21eb5fc
Jeremy Neander <subjectego@gmail.com>
2012-07-20 09:26:47 -0500

    Make 'score' return -1 for min_mark win

--- lib/minimax.rb
+++ lib/minimax.rb
@@ -2,6 +2,8 @@ class Minimax
   attr_accessor :min_mark, :max_mark
 
   def score(board,mark)
-    return 1
+    score = 1 if board.winning_solution?(@max_mark)
+    score = -1 if board.winning_solution?(@min_mark)
+    score
   end
 end
--- spec/minimax_spec.rb
+++ spec/minimax_spec.rb
@@ -6,12 +6,18 @@ describe Minimax do
     @minimax = Minimax.new
     @minimax.max_mark = :max_mark
     @minimax.min_mark = :min_mark
+    @board = Board.new
   end
 
   it "returns 1 for max_mark win" do
-    @board = Board.new
     @board.stub!(:winning_solution?).with(:min_mark).and_return(false)
     @board.stub!(:winning_solution?).with(:max_mark).and_return(true)
     @minimax.score(@board,:max_mark).should == 1
+  end
+
+  it "returns -1 for min_mark win" do
+    @board.stub!(:winning_solution?).with(:min_mark).and_return(true)
+    @board.stub!(:winning_solution?).with(:max_mark).and_return(false)
+    @minimax.score(@board,:max_mark).should == -1
   end
 end
