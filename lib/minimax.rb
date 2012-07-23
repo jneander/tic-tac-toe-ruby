c684bc96c6ca8307b4c27a5c40b08c033db7d5bd
Jeremy Neander <subjectego@gmail.com>
2012-07-23 11:04:50 -0500

    Incorporate 'scores' method into 'score' method

--- lib/minimax.rb
+++ lib/minimax.rb
@@ -7,21 +7,14 @@ class Minimax
     score = -1 if board.winning_solution?(@min_mark)
 
     if score == 0
-      available_spaces = board.spaces_with_mark(Mark::BLANK)
       next_mark = current_mark == @min_mark ? @max_mark : @min_mark
-      next_score = 0
 
-      available_spaces.each {|space|
-        board.make_mark(space,next_mark)
-        next_score = score(board,current_mark)
-        board.make_mark(space,Mark::BLANK)
-
-        if (next_score > score and next_mark == @max_mark) or
-           (next_score < score and next_mark == @min_mark) or
-           (space == available_spaces.first)
-          score = next_score
-        end
-      }
+      space_scores = scores(board, next_mark)
+      if not space_scores.empty?
+        space_scores = space_scores.sort_by {|key,value| value}.reverse
+        score = next_mark == @max_mark ? 
+          space_scores.first[1] : space_scores.last[1]
+      end
     end
 
     score
