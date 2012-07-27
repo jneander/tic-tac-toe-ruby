3d948a04059b4c10f59063541163e0b5ae27fba8
Jeremy Neander <subjectego@gmail.com>
2012-07-27 15:55:23 -0500

    Add list of 'Player' classes as array constant

--- lib/configuration.rb
+++ lib/configuration.rb
@@ -1,5 +1,11 @@
+require 'human'
+require 'computer_dumb'
+require 'computer_impossible'
+
 class Configuration
   attr_reader :console
+
+  PLAYER_CLASSES = [Human, DumbComputer, ImpossibleComputer]
 
   def initialize(console)
     @console = console
--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -1,9 +1,19 @@
 require 'configuration'
+require 'human'
+require 'computer_dumb'
+require 'computer_impossible'
 
 describe Configuration do
-  it "initializes with a 'Console' object" do
+  before :each do
     @console = mock("Console")
     @config = Configuration.new(@console)
+  end
+
+  it "initializes with a 'Console' object" do
     @config.console.should equal @console
+  end
+
+  it "contains a list of available 'Player' classes" do
+    Configuration::PLAYER_CLASSES.should == [Human, DumbComputer, ImpossibleComputer]
   end
 end
