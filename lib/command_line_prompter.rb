32171ba8ab39cf257a0967c7bf44ea6b36e1682e
Jeremy Neander <subjectego@gmail.com>
2012-07-28 13:25:47 -0500

    Make method to print a request to the output
    Method 'request' should receive one argument and send it to the
    'print' method of the output stream.

--- lib/command_line_prompter.rb
+++ lib/command_line_prompter.rb
@@ -1,6 +1,10 @@
 class CommandLinePrompter
-  def set_input_output(input, output)
-    @in = input
-    @out = output
+  def set_input_output(instream, outstream)
+    @in = instream
+    @out = outstream
+  end
+
+  def request(message)
+    @out.print(message)
   end
 end
--- spec/command_line_prompter_spec.rb
+++ spec/command_line_prompter_spec.rb
@@ -1,8 +1,18 @@
 require 'command_line_prompter'
 
 describe CommandLinePrompter do
-  it "receives references to input and output streams" do
+  before :all do
     @prompter = CommandLinePrompter.new
+  end
+
+  it "receives references to input and output streams" do
     @prompter.set_input_output($stdin, $stdout)
+  end
+
+  it "prints a request" do
+    @output = StringIO.new('','w')
+    @prompter.set_input_output(nil, @output)
+    @output.should_receive(:print)
+    @prompter.request("request!")
   end
 end
