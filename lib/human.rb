103c4534abe18ddb8dfefb69a0410a6df125fe10
Jeremy Neander <subjectego@gmail.com>
2012-07-31 09:28:08 -0500

    Extract #choose_move from #make_mark
    Split responsibilities on the Human player by separating the choosing of
    a space index to mark from the actual marking action.

--- lib/human.rb
+++ lib/human.rb
@@ -2,12 +2,17 @@ class Human
   attr_accessor :console
 
   def make_mark(board)
+    index = choose_move(board)
+    board.make_mark(index,self)
+  end
+
+  def choose_move(board)
     index = @console.prompt_player_mark
     while not board.space_available?(index)
       @console.alert_space_unavailable(index)
       index = @console.prompt_player_mark
     end
-    board.make_mark(index,self)
+    index
   end
 
   def self.to_s
--- spec/human_spec.rb
+++ spec/human_spec.rb
@@ -23,6 +23,17 @@ describe Human do
     @player.make_mark(@board)
   end
 
+  it "#choose_move requests input from the console" do
+    @console.should_receive("prompt_player_mark").and_return(0)
+    @player.choose_move(@board)
+  end
+
+  it "#choose_move checks for valid mark information" do
+    @console.stub!(:prompt_player_mark).and_return(0, 1)
+    @board.should_receive(:space_available?).and_return(false, true)
+    @player.choose_move(@board).should == 1
+  end
+
   it "converts the class to a string" do
     Human.to_s.should == "Human"
   end
