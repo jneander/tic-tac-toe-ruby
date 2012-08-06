66ef9ef813d5aa59bb0a082f693f1518cd7ddccd
Jeremy Neander <subjectego@gmail.com>
2012-08-06 00:29:50 -0500

    Change spacing to use more padding

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -25,18 +25,18 @@ describe Board do
   it "#initialize generates a set of winning solutions for board size 3" do
     board = Board.new(3)
     board.solutions.should == [
-      [0,1,2],[3,4,5],[6,7,8],
-      [0,3,6],[1,4,7],[2,5,8],
-      [0,4,8],[2,4,6]
+      [0, 1, 2], [3, 4, 5], [6, 7, 8],
+      [0, 3, 6], [1, 4, 7], [2, 5, 8],
+      [0, 4, 8], [2, 4, 6]
     ]
   end
 
   it "#initialize generates a set of winning solutions for board size 4" do
     board = Board.new(4)
     board.solutions.should == [
-      [0,1,2,3],[4,5,6,7],[8,9,10,11],[12,13,14,15],
-      [0,4,8,12],[1,5,9,13],[2,6,10,14],[3,7,11,15],
-      [0,5,10,15],[3,6,9,12]
+      [0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11], [12, 13, 14, 15],
+      [0, 4, 8, 12], [1, 5, 9, 13], [2, 6, 10, 14], [3, 7, 11, 15],
+      [0, 5, 10, 15], [3, 6, 9, 12]
     ]
   end
 
@@ -46,7 +46,7 @@ describe Board do
   end
 
   it "returns an array of spaces with the specified mark" do
-    target_spaces = [3,5,7,8]
+    target_spaces = [3, 5, 7, 8]
     make_marks(@board, target_spaces, :player)
     @board.spaces_with_mark(:player).should eql target_spaces
     expected_spaces = (0..8).to_a - target_spaces
@@ -68,20 +68,20 @@ describe Board do
 
   it "can check for winning solutions on multiple marks at once" do
     make_marks(@board, @board.solutions.first, :player1)
-    @board.winning_solution?(*[:player1,:player2]).should == true
+    @board.winning_solution?(*[:player1, :player2]).should == true
     @board.winning_solution?(:player1).should == true
     @board.winning_solution?(:player2).should == false
   end
 
   it "returns true if a queried space is valid and unmarked" do
-    @board.make_mark(1,:player)
+    @board.make_mark(1, :player)
     @board.space_available?(0).should == true
     @board.space_available?(1).should == false
     @board.space_available?(-1).should == false
   end
 
   it "#reset will set each space to BLANK mark" do
-    make_marks(@board, [0,1,2,3,4], :player)
+    make_marks(@board, [0, 1, 2, 3, 4], :player)
     @board.reset
     @board.spaces_with_mark(:player).should == []
     @board.spaces_with_mark(Board::BLANK).should == (0..8).to_a
@@ -92,7 +92,7 @@ describe Board do
   end
 
   it "#symbols_added stores player symbols added to the board" do
-    make_marks(@board, [1,3], :player1)
+    make_marks(@board, [1, 3], :player1)
     make_marks(@board, [0], :player2)
     @board.symbols_added.should == [:player2, :player1]
   end
@@ -100,8 +100,8 @@ describe Board do
   context "#clone" do
     before :all do
       @orig = Board.new
-      make_marks(@orig, [1,2], :player1)
-      make_marks(@orig, [3,4], :player2)
+      make_marks(@orig, [1, 2], :player1)
+      make_marks(@orig, [3, 4], :player2)
       @clone = @orig.clone
     end
 
